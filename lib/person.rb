ae89a4ddfbb47f35beba3ee56477a6230ef39f73
Omar Nabi <onabi20@gmail.com>
2020-04-24 02:36:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,9 @@
 class Person
  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize(attrbutes)
-  attributes.each do |key, value|
-    self.send(("#{key}="),value)
-  end
+def initialize(attributes)
+    attributes.each do |key, value|
+      self.se
 end
 
 
